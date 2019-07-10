cd0a3ff03df5a9fde73dc37d6865eeee64fc7363
JP <jpsala@gmail.com>
2019-07-09 22:10:58 -0300

    ...

--- src/components/MySpinner.vue
+++ src/components/MySpinner.vue
@@ -3,21 +3,9 @@
 </template>
 <script>
 export default {
-
 };
 </script>
 <style>
-.loading2{
-  position: fixed;
-  z-index: 999;
-  overflow: visible;
-  margin: auto;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  color: red;
-}
 .loading {
   position: absolute;
   top: 24vw;
